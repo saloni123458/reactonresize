1deae349e86a260f45ac69228727ea0813456eb5
SALONI GARJE <77056763+saloni123458@users.noreply.github.com>
2021-08-31 16:08:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useState } from "react";
-import "./styles.css";
-
+import React, { useState } from "react";
+import { withResizeDetector } from "react-resize-detector";
 const VIDEO_URLS = [
   "https://raw.githubusercontent.com/juanpablocs/react-vplayer/master/demo/video/720p.mp4",
   "https://raw.githubusercontent.com/juanpablocs/react-vplayer/master/demo/video/1080p.mp4"
 ];
+const containerStyles = {
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+};
 
-export default function App() {
+const AdaptiveComponent = ({ width, height }) => {
+  return (
+    <div style={{ ...containerStyles }}>
+      {console.log(`Video source resized to ${width}x${height}`)}
+    </div>
+  );
+};
+const AdaptiveWithDetector = withResizeDetector(AdaptiveComponent);
+
+const App = () => {
   const [videoIdx, setVideoIdx] = useState(0);
   return (
     <div className="App">
@@ -14,19 +28,18 @@ export default function App() {
         Missing <code>onResize</code> handler
       </h1>
       <p>Open the console to see a warning.</p>
+
       <video
         src={VIDEO_URLS[videoIdx]}
         controls
         onLoadedMetadata={(e) => console.log("Metadata loaded OK.")}
-        onResize={(e) =>
-          console.log(
-            `Video source resized to ${e.target.videoWidth}x${e.target.videoHeight}`
-          )
-        }
       />
       <button onClick={() => setVideoIdx((i) => (i + 1) % VIDEO_URLS.length)}>
         Change video resolution
       </button>
+      <AdaptiveWithDetector />
     </div>
   );
-}
+};
+
+export default App;
